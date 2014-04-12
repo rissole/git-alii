adc298a5babceb766ee9a5c33088bdf4317bc3f1
rissole <bonzaibrother@gmail.com>
2014-04-12 13:58:32 +1000

    Create git-alii.sh

---
+++ git-alii.sh
@@ -0,0 +1,8 @@
+# just source this bad boy and it's ready to go
+alias ga="git add"
+alias gc="git commit"
+alias gd="git diff"
+alias gco="git checkout"
+alias gp="git push"
+alias gl="git pull"
+alias gau="git add -u"
